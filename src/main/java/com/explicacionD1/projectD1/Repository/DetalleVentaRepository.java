daef49f5ffe1f95421bace3ccaaf6b5678984634
Santiago Rueda Garcia <santirueda0917@gmail.com>
2026-08-12 08:19:09 -0500

    Added @Repository to the respective classes

--- src/main/java/com/explicacionD1/projectD1/Repository/DetalleVentaRepository.java
+++ src/main/java/com/explicacionD1/projectD1/Repository/DetalleVentaRepository.java
@@ -2,9 +2,11 @@ package com.explicacionD1.projectD1.Repository;
 
 import com.explicacionD1.projectD1.Model.DetalleVenta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
 
     List<DetalleVenta> findByProductoId(Long id);
--- src/main/java/com/explicacionD1/projectD1/Repository/VentaRepository.java
+++ src/main/java/com/explicacionD1/projectD1/Repository/VentaRepository.java
@@ -4,10 +4,12 @@ import com.explicacionD1.projectD1.Model.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
 
+@Repository
 public interface VentaRepository extends JpaRepository<Venta, Long> {
     List<Venta> findByFecha(Date fecha);
 
